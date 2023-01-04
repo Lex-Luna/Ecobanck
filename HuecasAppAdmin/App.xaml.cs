f290ca364000d966c279e62aaab6f79063d9e8f3
AlexisLuna <alexisgabriel.lu@gmail.com>
2023-01-03 21:01:14 -0500

    Menu principal Admin

---
+++ HuecasAppAdmin/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using HuecasAppAdmin.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HuecasAppAdmin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MenPrincipalV();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
